9d9dd7afb35a2004a2079864e9abc585ded4d7b5
limin <13693576511@163.com>
2019-08-01 07:02:20 +0800

    根据老的状态和action，生成新的state

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {createStore} from 'redux'
+// 新建store
+// 通过reducer建立
+// 根据老的状态和action，生成新的state
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function counter(state=0,action){
+    switch(action.type){
+        case '加机关枪':
+        return state+1
+        case '减机关枪':
+        return state-1
+        default:
+        return 10
+    }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//  新建store
+const store=createStore(counter)
+
+const init = store.getState()
+console.log(init,'>>')
+
+function listener(){
+    const current = store.getState()
+    console.log(`现在有${current}把机关枪`)
+}
+store.subscribe(listener)// 订阅变化 // 10 11 10
+
+// 派发事件 传递action
+store.dispatch({type:'加机关枪'})
+console.log(store.getState())
+store.dispatch({type:'加机关枪'})
+console.log(store.getState())
+store.dispatch({type:'减机关枪'})
+console.log(store.getState())
